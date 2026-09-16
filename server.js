a386cecee9238b538dfc045379effc92a205246a
AlekseyBenetski <easygostyle09@gmail.com>
2026-09-16 17:10:53 +0300

    env deleted

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const documentsRouter = require('./routes/documents');
 const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
